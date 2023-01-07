f00f88a82e05adb8546bd0e0f2e74232213d1d92
Sahrul Dani Ardiansyah <daniardiansyah448@gmail.com>
2023-01-07 21:23:17 +0700

    Tugas 3 C++
    Dikumpulkan untuk memenuhi  tugas dari Niomic

---
+++ Tugas3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int A,B,C;
+	A=10;
+	B=20;
+	C=A*B;
+	cout<<"Nilai Variabel A adalah  : " <<A<<endl;
+	cout<<"Nilai Variabel A adalah  : " <<B<<endl;
+	cout<<"Hasil Perkalian Kedua Variabel Adalah : "<<C;
+}
